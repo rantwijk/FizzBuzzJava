5c1e76b54def505b88f167d836ac2a452bcc23fe
Charlie van Rantwijk <charlievanrantwijk@gmail.com>
2019-01-08 14:34:04 +0100

    initial commit

---
+++ src/fizzbuzz/main.java
@@ -0,0 +1,19 @@
+package fizzbuzz;
+
+public class main {
+	public static void main(String[] args) {
+		for(int i=1;i<=100;i++) {
+			if(i%3==0) {
+				if(i%5==0) {
+					System.out.println("fizzbuzz");
+				}else {
+					System.out.println("fizz");
+				}
+			}else if(i%5==0) {
+				System.out.println("buzz");
+			}else {
+				System.out.println(i);
+			}
+		}
+	}
+}
